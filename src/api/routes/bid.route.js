1610ebe912bb1696c4993d3ba74aa2f019924095
Durga Rao Pamarthi <durga.pamarthi@iddinkgroup.com>
2020-05-10 12:38:57 +0530

    bug fix

--- src/api/routes/bid.route.js
+++ src/api/routes/bid.route.js
@@ -5,7 +5,7 @@ const { create, query } = require('../controller/bid.controller');
 
 router.post('/query', async(req, res, next) => {
     const bids = await query(req.body);
-    res.status(200).json(requests);
+    res.status(200).json(bids);
 });
 
 
