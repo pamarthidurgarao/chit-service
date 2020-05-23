86151f5f8cb7b3810be46643d0eb470a0513c84c
Durga Rao Pamarthi <durga.pamarthi@iddinkgroup.com>
2020-05-23 14:05:55 +0530

    user model changes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -5,10 +5,14 @@ const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     firstName: { type: String, required: true },
     lastName: { type: String, required: false },
+    displayName: { type: String, required: false },
     email: { type: String, required: false },
     mobile: { type: String, required: false },
     gender: { type: String, required: false },
+    imageUrl: { type: String, required: false },
     dob: { type: String, required: false },
-    address: { type: String, required: false }
+    address: { type: String, required: false },
+    fbId: { type: String, required: false },
+    googleId: { type: String, required: false },
 });
 module.exports = mongoose.model('User', userSchema);
