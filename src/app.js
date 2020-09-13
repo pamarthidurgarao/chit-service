787a8d40e36835b7f59c45dadc29cb24af53d6d8
Durga Rao Pamarthi <durga.pamarthi@iddinkgroup.com>
2020-09-13 17:10:48 +0530

    init changes

--- src/app.js
+++ src/app.js
@@ -1,36 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const bodyParser = require('body-parser');
-const config = require('./config/init.config')
+const bodyParser = require("body-parser");
+const config = require("./config/init.config");
 
-const userRoute = require('./api/routes/user.route');
-const chitRoute = require('./api/routes/chit.route');
-const instalmentRoute = require('./api/routes/instalment.route');
-const chitRequestRoute = require('./api/routes/chit-request.route');
+const userRoute = require("./api/routes/user.route");
 
-const bidRoute = require('./api/routes/bid.route');
-
-var swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // DB init
 config.initializeDB();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const baseUrl = "/chit/api/v1";
+const baseUrl = "/teachit/api/v1";
 
-// cors setup 
+// cors setup
 app.use(config.cors);
 
 // Routes setup
-app.use(baseUrl + '/user', userRoute);
-app.use(baseUrl + '/chit', chitRoute);
-app.use(baseUrl + '/instalment', instalmentRoute);
-app.use(baseUrl + '/chitRequest', chitRequestRoute);
-app.use(baseUrl + '/bid', bidRoute);
+app.use(baseUrl + "/user", userRoute);
 
 module.exports = app;
