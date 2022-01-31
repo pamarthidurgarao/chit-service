0dc56a08a402123282f4e06cb477c6ea81528e6f
Siri <chandanaandige506@gmail.com>
2022-01-31 10:51:41 +0530

    db config changes

--- src/config/init.config.js
+++ src/config/init.config.js
@@ -1,22 +1,28 @@
-const mongoose = require('mongoose');
-const { mongoUrl } = require('./index');
+const mongoose = require("mongoose");
+const { mongoUrl } = require("./index");
 
 module.exports = {
-    initializeDB: async() => {
-        mongoose.connect('mongodb://chitdb:Welcome1@ds149844.mlab.com:49844/chit-db', { useNewUrlParser: true, useUnifiedTopology: true });
-        // mongoose.Promise = global.Promise;
-    },
+  initializeDB: async () => {
+    mongoose.connect(
+      "mongodb+srv://pamarthid:qwerty12345@cluster0.1lb0u.mongodb.net/chit-db?retryWrites=true&w=majority",
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    );
+    // mongoose.Promise = global.Promise;
+  },
 
-    cors: async(req, res, next) => {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header(
-            "Access-Control-Allow-Headers",
-            "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-        );
-        if (req.method === "OPTIONS") {
-            res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-            return res.status(200).json({});
-        }
-        next();
+  cors: async (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+      res.header(
+        "Access-Control-Allow-Methods",
+        "PUT, POST, PATCH, DELETE, GET"
+      );
+      return res.status(200).json({});
     }
-}
+    next();
+  },
+};
