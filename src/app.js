bca985acc94d391ba76cd4f1f7f54df341bd3ce7
Siri <chandanaandige506@gmail.com>
2022-02-05 15:11:38 +0530

    cors issue fix

--- src/app.js
+++ src/app.js
@@ -1,20 +1,21 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
-const bodyParser = require('body-parser');
-const config = require('./config/init.config')
+const bodyParser = require("body-parser");
+const config = require("./config/init.config");
 
-const userRoute = require('./api/routes/user.route');
-const chitRoute = require('./api/routes/chit.route');
-const instalmentRoute = require('./api/routes/instalment.route');
-const chitRequestRoute = require('./api/routes/chit-request.route');
+const userRoute = require("./api/routes/user.route");
+const chitRoute = require("./api/routes/chit.route");
+const instalmentRoute = require("./api/routes/instalment.route");
+const chitRequestRoute = require("./api/routes/chit-request.route");
 
-const bidRoute = require('./api/routes/bid.route');
+const bidRoute = require("./api/routes/bid.route");
 
-var swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+var swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // DB init
 config.initializeDB();
 
@@ -23,14 +24,19 @@ app.use(bodyParser.json());
 
 const baseUrl = "/chit/api/v1";
 
-// cors setup 
-app.use(config.cors);
+// cors setup
+// app.use(config.cors);
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 // Routes setup
-app.use(baseUrl + '/user', userRoute);
-app.use(baseUrl + '/chit', chitRoute);
-app.use(baseUrl + '/instalment', instalmentRoute);
-app.use(baseUrl + '/chitRequest', chitRequestRoute);
-app.use(baseUrl + '/bid', bidRoute);
+app.use(baseUrl + "/user", userRoute);
+app.use(baseUrl + "/chit", chitRoute);
+app.use(baseUrl + "/instalment", instalmentRoute);
+app.use(baseUrl + "/chitRequest", chitRequestRoute);
+app.use(baseUrl + "/bid", bidRoute);
 
 module.exports = app;
