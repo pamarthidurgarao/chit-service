73819624a2ba7ba08adf712b25f7a21153a14dd7
Siri <chandanaandige506@gmail.com>
2022-02-07 13:04:37 +0530

    installment changes

--- src/models/instalment.model.js
+++ src/models/instalment.model.js
@@ -1,13 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const instalmentSchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    amount: { type: Number, required: true },
-    chitId: { type: mongoose.Schema.Types.ObjectId, required: true },
-    instalmentDate: { type: Date, required: true },
-    chitDate: { type: Date, required: true },
-    instalmentNumber: { type: Number, required: true },
-    paidUser: { type: Schema.Types.ObjectId, ref: 'User' }
+  _id: mongoose.Schema.Types.ObjectId,
+  amount: { type: Number, required: true },
+  chitId: { type: mongoose.Schema.Types.ObjectId, required: true },
+  instalmentDate: { type: Date, required: true },
+  chitDate: { type: Date, required: true },
+  instalmentNumber: { type: Number, required: true },
+  users: [
+    {
+      user: { type: Schema.Types.ObjectId, ref: "User" },
+      paymentStatus: { type: String },
+      paymentDate: { type: Date },
+    },
+  ],
 });
-module.exports = mongoose.model('Instalment', instalmentSchema);
+module.exports = mongoose.model("Instalment", instalmentSchema);
