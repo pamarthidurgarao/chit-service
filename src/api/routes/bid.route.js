012d5bfe0fb25ea9a90ae6b7d107e479cf4af133
Siri <chandanaandige506@gmail.com>
2022-02-09 18:13:51 +0530

    delete bid

--- src/api/routes/bid.route.js
+++ src/api/routes/bid.route.js
@@ -1,22 +1,30 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { create, query, socketTest } = require('../controller/bid.controller');
+const {
+  create,
+  query,
+  socketTest,
+  deleteBid,
+} = require("../controller/bid.controller");
 
-
-router.post('/query', async(req, res, next) => {
-    const bids = await query(req.body);
-    res.status(200).json(bids);
+router.post("/query", async (req, res, next) => {
+  const bids = await query(req.body);
+  res.status(200).json(bids);
 });
 
-
-router.post("/", async(req, res, next) => {
-    const bid = await create(req.body)
-    res.status(201).json(bid)
+router.post("/", async (req, res, next) => {
+  const bid = await create(req.body);
+  res.status(201).json(bid);
 });
 
-router.get("/:id", async(req, res, next) => {
-    await socketTest(req.params.id)
-    res.status(201).json();
+router.get("/:id", async (req, res, next) => {
+  await socketTest(req.params.id);
+  res.status(201).json();
+});
+
+router.delete("/:id", async (req, res, next) => {
+  await deleteBid(req.params.id);
+  res.status(200).json();
 });
 
 module.exports = router;
